2a44f17769b77e68aa6f6fde69e0ced204a4598e
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-09 23:41:50 +0900

    Refactor ping

--- src/ping.rs
+++ src/ping.rs
@@ -1,14 +1,21 @@
-use redis::{Client};
+use redis::{Client, RedisError};
+use redis::aio::Connection;
+
+async fn create_redis_connection() -> Result<Connection, RedisError> {
+    let client = Client::open("redis://127.0.0.1/")?;
+    client.get_async_connection().await
+}
+
+async fn ping_request() -> Result<String, RedisError> {
+    let mut connection = create_redis_connection().await?;
+    redis::cmd("PING").arg("hello")
+        .query_async(&mut connection)
+        .await
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::open("redis://127.0.0.1/")?;
-    let mut connection = client.get_async_connection().await?;
-    let response: String =
-        redis::cmd("PING")
-            .arg("hello")
-            .query_async(&mut connection)
-            .await?;
+    let response = ping_request().await?;
     println!("{}", response);
     Ok(())
 }
