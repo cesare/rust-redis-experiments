04580abba56ebcf525cf5a515bafa79cfcea7d8b
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-09 21:37:46 +0900

    Make ping async

--- src/ping.rs
+++ src/ping.rs
@@ -1,9 +1,14 @@
 use redis::{Client};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::open("redis://127.0.0.1/")?;
-    let mut connection = client.get_connection()?;
-    let response = redis::cmd("PING").arg("hello").query::<String>(&mut connection)?;
+    let mut connection = client.get_async_connection().await?;
+    let response: String =
+        redis::cmd("PING")
+            .arg("hello")
+            .query_async(&mut connection)
+            .await?;
     println!("{}", response);
     Ok(())
 }
