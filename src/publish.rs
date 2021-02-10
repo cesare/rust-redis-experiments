7d34203233390d72819829eec71680f8fd6008f7
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-11 01:54:42 +0900

    Enable to configure channel

--- src/publish.rs
+++ src/publish.rs
@@ -1,14 +1,26 @@
 use redis::{AsyncCommands, RedisError};
+use structopt::StructOpt;
 
 mod redis_client;
 
-async fn publish_request() -> Result<(), RedisError> {
+#[derive(StructOpt)]
+#[structopt(name = "publish")]
+struct PublishConfig {
+    #[structopt(short = "c", long = "channel", default_value = "channel-a")]
+    channel: String,
+
+    #[structopt(name = "message")]
+    message: String,
+}
+
+async fn publish_request(config: &PublishConfig) -> Result<(), RedisError> {
     let mut connection = redis_client::create_connection().await?;
-    connection.publish("channel-a", "test for echo").await
+    connection.publish(&config.channel, &config.message).await
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    publish_request().await?;
+    let config = PublishConfig::from_args();
+    publish_request(&config).await?;
     Ok(())
 }
--- src/subscribe.rs
+++ src/subscribe.rs
@@ -1,12 +1,22 @@
 use futures_util::stream::StreamExt;
+use structopt::StructOpt;
 
 mod redis_client;
 
+#[derive(StructOpt)]
+#[structopt(name = "subscribe")]
+struct SubscriptionConfig {
+    #[structopt(short = "c", long = "channel", default_value = "channel-a")]
+    channel: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = SubscriptionConfig::from_args();
+
     let connection = redis_client::create_connection().await?;
     let mut pubsub = connection.into_pubsub();
-    pubsub.subscribe("channel-a").await?;
+    pubsub.subscribe(&config.channel).await?;
 
     let mut stream = pubsub.on_message();
     while let Some(message) = stream.next().await {
